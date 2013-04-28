65da76b474f3ed0e88e3f17877ad16559673d9c7
Joseph Utecht <thenmal@gmail.com>
2013-04-27 19:48:36 -0500

    idiort moving

---
+++ Assets/Scripts/idiort_move.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class idiort_move : MonoBehaviour {
+	private GameObject[] idiorts;
+	private GameObject[] navs;
+	// Use this for initialization
+	void Start () {
+		idiorts = GameObject.FindGameObjectsWithTag("idiort");
+		navs = GameObject.FindGameObjectsWithTag("nav");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.B)){
+			int i = 0;
+			foreach(GameObject idiort in idiorts){
+				NavMeshAgent n = idiort.GetComponent<NavMeshAgent>();
+				Transform dest = navs[i].GetComponent<Transform>();
+				i++;
+				n.SetDestination(dest.position);
+			}
+		}
+	}
+}
